c529effd2110b1c1185e807f1f82d7afe8229783
tvaz085-prog <tvaz085@gmail.com>
2026-03-23 20:02:34 -0300

    Update Evento.java

--- Evento.java
+++ Evento.java
@@ -1,1 +1,23 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
+public class Evento {
+    String nome;
+    String endereco;
+    String cidade;
+    String categoria;
+    LocalDateTime data;
+    String descricao;
+
+    ArrayList<String> participantes = new ArrayList<>();
+
+    public Evento(String nome, String endereco, String cidade,
+                  String categoria, LocalDateTime data, String descricao) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.categoria = categoria;
+        this.data = data;
+        this.descricao = descricao;
+    }
+}
