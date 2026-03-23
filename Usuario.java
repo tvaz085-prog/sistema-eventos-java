92961fe339a38e650ca39aacacabbd7745d5d158
tvaz085-prog <tvaz085@gmail.com>
2026-03-23 20:03:21 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,1 +1,11 @@
+public class Usuario {
+    String nome;
+    String email;
+    String cidade;
 
+    public Usuario(String nome, String email, String cidade) {
+        this.nome = nome;
+        this.email = email;
+        this.cidade = cidade;
+    }
+}
